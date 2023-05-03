ee975c40a5faa002945b35c729db308acfa7bdea
VALERIAJENNERY <valeriajennery@gmail.com>
2023-05-02 21:31:45 -0500

    PRIMER AVANCE :+1:

---
+++ app.py
@@ -0,0 +1,9 @@
+from back.areas import *
+
+print("Estamos haciendo nuestro primer código organizando rutas")
+
+#nuestro objetivo es importar rutas desde otro archivos y organizar nuestro código.
+
+circle(9)
+
+square(10)
---
+++ back/areas.py
@@ -0,0 +1,9 @@
+import math
+
+#Calculate the area of a circle
+def circle(radius):
+    print(math.pi * radius ** 2)
+
+#Calculate the area of a square
+def square(side):
+    print(side * side)
